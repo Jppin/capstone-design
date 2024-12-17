6ce95ce9e3e7b57792c2b4252da342686b512580
유현서 <pipi2938@naver.com>
2024-12-17 16:41:56 +0900

    OCR로직 구현 업로드

--- start_final/config/config.py
+++ start_final/config/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 langchain_tracing_v2 = os.getenv("LANGCHAIN_TRACING_V2")
 langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
 openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_url = os.getenv("OPENAI_API_URL")
 mongo_uri = os.getenv("MONGO_URI")
 db_name = os.getenv("DB_NAME")
 
